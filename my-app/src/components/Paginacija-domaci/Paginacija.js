9f050d53f7c0f1b5e51f8b847f5cb7bf079f10f6
medinalicina <medinalicina94@gmail.com>
2023-10-16 17:57:12 +0200

    domaci

--- my-app/src/components/Paginacija-domaci/Paginacija.js
+++ my-app/src/components/Paginacija-domaci/Paginacija.js
@@ -1,21 +1,31 @@
 import { useState, useEffect} from "react";
-import "./Post.css";
-import "./Paginacija.css";
+import "./post.css";
+import "./paginacija.css";
 import Post from "./post";
-
 
 function Paginacija() {
     const [defaultData, setDefaultData] = useState([]);
+    const [paginacija, setPaginacija] = useState(0);
+
+  const scrollToTop = () => {
+    window.scrollTo({top: 0, behavior: "smooth"})
+  }
+
     useEffect(() => {
-       fetch('https://dummyjson.com/posts?limit=10&skip=10&select=title,reactions,body')
+       fetch(`https://dummyjson.com/posts?skip=${paginacija}`)
          .then(res => res.json())
          .then((data) => {
             setDefaultData(data?.posts);
             console.log(data);
          });
          console.log(defaultData);
-    }, []);
+       
+    }, [paginacija]);
 
+    const handlePaginacijaClick = (page) => {
+      setPaginacija(page);
+    };  
+    
     return(
         <div className="paginacija">
       {defaultData?.length > 0 ? (
@@ -23,9 +33,18 @@ function Paginacija() {
       ) : (
         <h1>Nema podataka</h1>
       )}
-
-      <div className="paginaton"></div>
-    </div>
+    <div className="pagination">
+    {[1, 2, 3, 4, 5].map((page) => (
+      <button
+        key={page}
+        className={paginacija === page - 1 ? "active" : ""}
+        onClick={() => handlePaginacijaClick(page - 1)}
+      >
+      {page} 
+      </button>
+          ))}
+      </div>
+      </div>
   );
 
  }
